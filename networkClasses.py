8e0f6a6a6d843c876e4c012b81e46525e5e2d129
LïamA <augu.liam@gmail.com>
2025-06-30 15:28:53 +0200

    feat : méthode global server cost

--- networkClasses.py
+++ networkClasses.py
@@ -6,11 +6,17 @@ class Server:
         
     def bind(self, others : list[object]):
         for machine in others:
-            self.client.append(machine.nom)
+            self.client.append(machine)
     
     def unbind(self, others : list[object]):
         for machine in others:
-            self.client.remove(machine.nom)
+            self.client.remove(machine)
+
+    def global_server_cost(self) -> int:
+        sum = 0
+        for machine in self.client:
+            sum += machine.prix
+        return sum + self.prix
     
     def __str__(self):
         return f"Nom du server : {self.nom}\nPrix du serveur :{self.prix}\nClient connecté au serveur :{self.client.nom}\n "
@@ -42,14 +48,14 @@ class Network:
         for client in range (len(other.client)):
             for port in range(len(self.port_sortie)) :
                 if self.port_sortie[port] == 0:
-                    self.port_sortie[port] = other.client[client]
+                    self.port_sortie[port] = other.client[client].nom
                     break
                 
     
     def delete_server(self, other: object):
         # Entrées
         for port in range(len(self.port_entree)) :
-            if self.port_entree[port] == other.nom:
+            if self.port_entree[port] == other:
                 self.port_entree[port] = 0
                 break
         # Sorties
