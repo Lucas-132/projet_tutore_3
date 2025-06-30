d12503af2ee03f41df1be75cbf845083810c81e2
LïamA <augu.liam@gmail.com>
2025-06-30 11:57:01 +0200

    change

--- networkClasses.py
+++ networkClasses.py
@@ -9,38 +9,23 @@ class Server:
     
     def unbind(self):
         self.client = None
-    
-<<<<<<< HEAD
-=======
+
     def __str__(self):
         return f"Nom du server : {self.nom}\n Prix du serveur :{self.prix}\n Client connecté au serveur :{self.client}\n "
     
->>>>>>> develop
 class Client(Server):
     
     def __init__(self, nom : str, prix:int=1000):
         super().__init__(nom, prix)
         self.nom = nom
         self.prix = prix
-<<<<<<< HEAD
-=======
-    
+
     def __str__(self):
         return f"Nom du PC : {self.nom}\n Prix du PC :{self.prix}\n"
->>>>>>> develop
         
 class Network:
     
     def __init__(self):
-<<<<<<< HEAD
-        pass
-    
-    def add_server(self, other: object):
-        pass
-    
-    def delete_server(self, other: object):
-        pass
-=======
         self.port_entree = [0 for x in range(3)]
         self.port_sortie = [0 for x in range(16)]
 
@@ -54,5 +39,4 @@ class Network:
         for port in range(len(self.port_entree)) :
             if self.port_entree[port] == other.nom:
                 self.port_entree.insert(port, 0)
->>>>>>> develop
     
