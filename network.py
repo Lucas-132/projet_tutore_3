856854e4b69dea076698811211950c67fb77dfbe
LïamA <augu.liam@gmail.com>
2025-06-30 11:11:36 +0200

    class Server and Client : try 1

--- network.py
+++ network.py
@@ -2,17 +2,20 @@ class Server:
     def __init__ (self, nom:str, prix:int= 5000):
         self.nom = nom
         self.prix = prix
+        self.client : object = None
         
     def bind(self, other : object):
-        pass
+        self.client = other
     
-    def unbind(self, other : object):
-        pass
+    def unbind(self):
+        self.client = None
     
 class Client(Server):
     
-    def __init__(self, nom : str, prix:int):
-        super().__init__(nom, prix=1000)    
+    def __init__(self, nom : str, prix:int=1000):
+        super().__init__(nom, prix)
+        self.nom = nom
+        self.prix = prix
         
 class Network:
     
