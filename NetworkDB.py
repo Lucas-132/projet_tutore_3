21c9a7fd18cbd88147f47912c945b5d92381971d
Romainducos <ducosromain31@gmail.com>
2025-07-01 15:31:02 +0200

    DB merged

--- NetworkDB.py
+++ NetworkDB.py
@@ -1,61 +1,63 @@
 import sqlite3
-import networkClasses
+import networkSystem
+
+def create_database():
+
+    net = networkSystem.Network("IPI1-DEV")
+    serv1 = networkSystem.Server("S1")
+    serv2 = networkSystem.Server("S2")     
+    serv3 = networkSystem.Server("S3")
+
+    conn = sqlite3.connect('NetworkDB1.db')
+    cur = conn.cursor()
 
 
-
-net = networkClasses.Network() 
-
-
-conn = sqlite3.connect('NetworkDB.db')
-cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS Client")
+    cur.execute("DROP TABLE IF EXISTS Server")
+    cur.execute("DROP TABLE IF EXISTS Network")
 
 
-cur.execute("DROP TABLE IF EXISTS Client")
-cur.execute("DROP TABLE IF EXISTS Server")
-cur.execute("DROP TABLE IF EXISTS Network")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Client (
+        name TEXT PRIMARY KEY,
+        price INT,
+        port_output TEXT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Server (
+        name TEXT PRIMARY KEY,
+        price INT,
+        ClientsNameTable TEXT,
+        port_input TEXT,
+        TotalPriceServ INT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Network (
+        ListPort TEXT, 
+        TotalPriceNet INT
+    )
+    """)
 
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS Client (
-    name TEXT PRIMARY KEY,
-    price INT,
-    port_output TEXT
-)
-""")
+    clients = [(f'C{i}', 1000, net.port_output[i-1]) for i in range(1, 17)]
+    cur.executemany("INSERT INTO Client (name, price, port_output) VALUES (?, ?, ?)", clients)
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS Server (
-    name TEXT PRIMARY KEY,
-    price INT,
-    ClientsNameTable TEXT,
-    port_input TEXT,
-    TotalPriceServ INT
-)
-""")
+    servers = [
+        (serv1.name, net.port_input[0], 5000, serv1.client, net.port_input, serv1.price),
+        (serv2.name, net.port_input[1], 5000,serv2.client , net.port_input, serv2.price),
+        (serv3.name, net.port_input[2], 5000,serv3.client , net.port_input, serv3.price)
+    ]
+    cur.executemany("INSERT INTO Server (name, price, ClientsNameTable, port_input, TotalPriceServ) VALUES (?, ?, ?, ?, ?)", servers)
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS Network (
-    ListPort TEXT, 
-    TotalPriceNet INT
-)
-""")
+    network = [
+        (str(net.port_output), 18000),
+    ]
+    cur.executemany("INSERT INTO Network (ListPort, TotalPriceNet) VALUES (?, ?)", network)
 
-
-clients = [(f'C{i}', 1000, net.port_output[i-1]) for i in range(1, 17)]
-cur.executemany("INSERT INTO Client (name, price, port_output) VALUES (?, ?, ?)", clients)
-
-servers = [
-    ('S1', 5000, 'C1', str(net.port_input), 6000),
-    ('S2', 5000, 'C2', str(net.port_input), 6000),
-    ('S3', 5000, 'C3', str(net.port_input), 6000),
-]
-cur.executemany("INSERT INTO Server (name, price, ClientsNameTable, port_input, TotalPriceServ) VALUES (?, ?, ?, ?, ?)", servers)
-
-network = [
-    (str(net.port_output), 18000),
-]
-cur.executemany("INSERT INTO Network (ListPort, TotalPriceNet) VALUES (?, ?)", network)
-
-conn.commit()
-cur.close()
-conn.close()
+    conn.commit()
+    cur.close()
+    conn.close()
