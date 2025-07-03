8ec2567d68fe18449880e688a437ba3297905605
LïamA <augu.liam@gmail.com>
2025-07-03 11:16:09 +0200

    main refait version high level, ne marche pas encore avec la bdd

--- logSystem.py
+++ logSystem.py
@@ -1,3 +1,37 @@
+import json
+from networkSystem import *
+from networkView import *
+
+
 class LogSystem:
-  def __init__(self):
-    pass
+  def __init__(self, configjson: str = "config.json"):
+    self.configjson = configjson
+  
+  def log(self):
+    # Chargement du JSON
+    with open(self.configjson, "r") as f:
+        config = json.load(f)
+    # Création du réseau
+    global network
+    network = Network(config["name"])
+
+    for server_data in config["servers"]:
+        server = Server(server_data["name"], server_data.get("cost", 0))
+        
+        # Ajout de l'imprimante
+        p = server_data.get("printer")
+        if p:
+            printer = Printer(p["name"], p["ip"], p["cost"])
+            server.bind([printer])
+        
+        # Ajout des clients
+        for c in server_data.get("clients", []):
+            client = Client(c["name"], c.get("cost", 0))
+            server.bind([client])
+        
+        network.add_server(server)
+    
+  def launch(self):
+    view = networkView([network])
+    print(view)
+    view.graph()
--- main.py
+++ main.py
@@ -3,40 +3,18 @@ from networkView import *
 from networkDB import create_database
 import os
 import json
+from logSystem import *
 
 if __name__ == "__main__":
     try:
-        # Chargement du JSON
-        with open("config.json", "r") as f:
-            config = json.load(f)
+        network = LogSystem("config.json")
+        network.log()
+        network.launch()
 
-        # Création du réseau
-        network1 = Network(config["name"])
-
-        for server_data in config["servers"]:
-            server = Server(server_data["name"], server_data.get("cost", 0))
-            
-            # Ajout de l'imprimante
-            p = server_data.get("printer")
-            if p:
-                printer = Printer(p["name"], p["ip"], p["cost"])
-                server.bind([printer])
-            
-            # Ajout des clients
-            for c in server_data.get("clients", []):
-                client = Client(c["name"], c.get("cost", 0))
-                server.bind([client])
-            
-            network1.add_server(server)
-
-        db_path = "NetworkDB1.db"
+        """db_path = "NetworkDB1.db"
         if os.path.exists(db_path):
             os.remove(db_path)
-        create_database(network1)
-        
-        view = networkView([network1])
-        print(view)
-        view.graph()
+        create_database(network)"""
         
     except Exception as e:
         print("\t", str(e))
